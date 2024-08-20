f5615d6c33a65e3dbe2042d04a2c26085f1ec03b
Gmorais-dev <guilherme_morais14@hotmail.com>
2024-08-20 09:44:02 -0300

    entitys

--- src/main/java/com/gerencimaneto_endereco/projeto/gerenciamento/de/endereco/Model/Address.java
+++ src/main/java/com/gerencimaneto_endereco/projeto/gerenciamento/de/endereco/Model/Address.java
@@ -1,4 +1,62 @@
 package com.gerencimaneto_endereco.projeto.gerenciamento.de.endereco.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return Street;
+    }
+
+    public void setStreet(String street) {
+        Street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    private String Street;
+
+    private String city;
+
+    private String zipcode;
+
+    public Address(long id, String street, String city, String zipcode) {
+        this.id = id;
+        Street = street;
+        this.city = city;
+        this.zipcode = zipcode;
+    }
+
+    public Address() {
+
+    }
 }
